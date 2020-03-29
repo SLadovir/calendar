ee9d24da350494b6e6f32dd14bcefeb51cb82241
SLadovir <sladovir@mail.ru>
2020-03-29 20:12:02 +0300

    Modified templates to display events from database.

--- wcal/views.py
+++ wcal/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Event
 # Create your views here.
+# кароч эта штука отвечает за представление всяких событий
 def event_list(request):
-    return render(request, 'wcal/event_list.html', {})
+    events = Event.objects.filter(event_date__lte=timezone.now()).order_by('event_date')
+    #dg-posts-events
+    return render(request, 'wcal/event_list.html', {'events': events})
     # (исправления в двух местах !!!)в dg написано post_list вместо event_list
     # в dg написано blog вместо wcal
+"""
+чтобы протестить что-то на командной строке
+командная строка:
+    python manage.py shell
+импорт класса Event:
+    from wcal.models import Event
+    from django.utils import timezone
+    Event.objects.filter(event_date__lte=timezone.now())
+
+"""
