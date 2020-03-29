e1436525e89861a9a152172cfd54689631e77994
SLadovir <sladovir@mail.ru>
2020-03-29 23:36:20 +0300

    Added views to create/(edit(?)) blog post inside the site.

--- wcal/urls.py
+++ wcal/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.event_list, name='event_list'), # (исправления в двух местах !!!)в dg написано post_list вместо event_list
+    path('event/<int:pk>/', views.event_detail, name='event_detail'),
+    path('event/new/', views.event_new, name='event_new'),
 ]
--- wcal/views.py
+++ wcal/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Event
+from django.shortcuts import render, get_object_or_404
+from .forms import EventForm #для формы добавления события (из текущего каталога)
+from django.shortcuts import redirect #для того чтобы после редактирования события мы сразу переходили к деталям события
+
 # Create your views here.
 # кароч эта штука отвечает за представление всяких событий
 def event_list(request):
@@ -9,6 +13,27 @@ def event_list(request):
     return render(request, 'wcal/event_list.html', {'events': events})
     # (исправления в двух местах !!!)в dg написано post_list вместо event_list
     # в dg написано blog вместо wcal
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'wcal/event_detail.html', {'event': event})
+
+def event_new(request):
+    form = EventForm()
+    return render(request, 'wcal/event_edit.html', {'form': form})
+
+def event_new(request):
+    if request.method == "POST": #тут не надо переимменовывать POST!!!! (это метод)
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            #event.author = request.user - это нам не нужно, но пусть будет
+            event.created_date = timezone.now()
+            event.save()
+            return redirect('event_detail', pk=event.pk)
+    else:
+        form = EventForm()
+    return render(request, 'wcal/event_edit.html', {'form': form})
+
 """
 чтобы протестить что-то на командной строке
 командная строка:
